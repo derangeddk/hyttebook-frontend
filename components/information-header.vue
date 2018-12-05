29f93238f9062f0f7fee1b1fe44d4cae790a2ef6
ITViking <philip.dein@gmail.com>
2018-12-05 17:14:29 +0100

    Restructure the header - Containerize all elements to allow for flexboxing - Flexbox everything - Make use of SASS - Adjust styling to the now flexboxed elements

--- components/information-header.vue
+++ components/information-header.vue
@@ -1,7 +1,16 @@
 <template>
     <nav>
-        <div class="hutname">{{ hutName }}</div>
-        <div class="username">{{ username }}</div>
+        <div class="header-wrapper">
+            <div class="site-name">
+                <h1>
+                    Hytte Index
+                </h1>
+            </div>
+            <div class="logged-in-user-information">
+                <div class="username">{{ username }}</div>
+                <div class="hutname">{{ hutName }}</div>
+            </div>
+        </div>
     </nav>
 </template>
 
@@ -28,32 +37,51 @@
     }
 </script>
 
-<style>
+<style lang="scss">
     nav {
-        top: 0;
         width: 100%;
         height: 60px;
-        margin: 0%;
-        position: absolute;
-        background-color: #772f73;
+        background-color: darken(#3C6E71, 5%);
         text-align: center;
-        font-family: Arial, Helvetica, sans-serif;
-        font-weight: bold;
+        color: rgb(230,230,230);
+    }
+
+    .header-wrapper {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-content: center;
+        align-items: center;
+    }
+
+    .site-name {
+        display: flex;
+    }
+
+    .site-name h1 {
+        align-self: flex-start;
+        font-size: 2.3em;
+        padding: 0.2em 0.5em;
+        margin: 0;
+    }
+
+    .logged-in-user-information {
+        display: flex;
+        flex-direction: column;
+        justify-content:flex-start;
+        align-items: center;
+        line-height: 100%;
     }
 
     .hutname {
-        padding-top: 20px;
-        padding-bottom: 20px;
-        display: inline-block;
-        position: relative;
+        padding: 0 1.2em;
+        font-weight: 300;
+        font-size: 0.95em;
     }
 
     .username {
-        padding-top: 20px;
-        padding-bottom: 20px;
-        display: inline-block;
-        float: right;
-        padding-right: 30px;
+        padding: 0 1.2em;
+        font-size: 1.1em;
     }
 </style>
 
