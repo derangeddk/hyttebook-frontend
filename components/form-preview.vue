bcfa4c6ee18bcbb2836dfcd46716f85f284dcb45
ITViking <philip.dein@gmail.com>
2018-12-05 17:07:47 +0100

    Extract form-preview and alter its structure - Replace ID's with name - Remove the unutilized for="" in every element - Specify the fieldnames' element type - Make use of SASS in the styling

--- components/form-preview.vue
+++ components/form-preview.vue
@@ -17,98 +17,96 @@
 
                 </div>
             </div>
-
-            <label for="form-org-type" v-if="showOrgType">
-                Organisations type
-                <input type="text" id="form-org-type">
+            <label v-if="showOrgType">
+                <span>Organisations type</span>
+                <input type="text" name="form-org-type">
             </label>
 
-            <label for="form-org-name">
-                Organisations navn
-                <input type="text" id="form-org-name" placeholder="DDS">
+            <label>
+                <span>Organisations navn</span>
+                <input type="text" name="form-org-name" placeholder="DDS">
             </label>
 
-            <label for="form-firstname">
-                Fornavn
-                <input type="text" id="form-firstname" placeholder="Birgitte">
+            <label>
+                <span>Fornavn</span>
+                <input type="text" name="form-firstname" placeholder="Birgitte">
             </label>
 
-            <label for="form-lastname">
-                Efternavn
-                <input type="text" id="form-lastname" placeholder="Hansen">
+            <label>
+                <span>Efternavn</span>
+                <input type="text" name="form-lastname" placeholder="Hansen">
             </label>
 
-            <label for="form-address">
-                Adresse
-                <input type="text" id="form-address" placeholder="Vejnavn og nr">
+            <label>
+                <span>Adresse</span>
+                <input type="text" name="form-address" placeholder="Vejnavn og nr">
             </label>
 
-            <label for="form-zipcode">
-                Postnummer
-                <input type="text" id="form-zipcode" placeholder="2500">
+            <label>
+                <span>Postnummer</span>
+                <input type="text" name="form-zipcode" placeholder="2500">
             </label>
 
-            <label for="form-city">
-                By
-                <input type="text" id="form-city" placeholder="Valby">
+            <label>
+                <span>By</span>
+                <input type="text" name="form-city" placeholder="Valby">
             </label>
 
-            <label for="form-country">
-                Land
-                <input type="text" id="form-country" placeholder="Denmark">
+            <label>
+                <span>Land</span>
+                <input type="text" name="form-country" placeholder="Denmark">
             </label>
 
-            <label for="form-phone-number">
-                Telefon
-                <input type="text" id="form-phone-number">
+            <label>
+                <span>Telefon</span>
+                <input type="text" name="form-phone-number">
             </label>
 
-            <label for="form-bank-name" v-if="showBankName">
-                Bank navn
-                <input type="text" id="form-bank-name" placeholder="Nordea">
+            <label v-if="showBankName">
+                <span>Bank navn</span>
+                <input type="text" name="form-bank-name" placeholder="Nordea">
             </label>
 
-            <label for="form-reg-number" v-if="showRegistration">
-                Reg
-                <input type="text" id="form-reg-number">
+            <label v-if="showRegistration">
+                <span>Reg</span>
+                <input type="text" name="form-reg-number">
             </label>
 
-            <label for="form-acc-number" v-if="showAccount">
-                Konto
-                <input type="text" id="form-acc-number">
+            <label v-if="showAccount">
+                <span>Konto</span>
+                <input type="text" name="form-acc-number">
             </label>
 
-            <label for="form-ean-number" v-if="showEan">
-                EAN
-                <input type="text" id="form-ean-number">
+            <label v-if="showEan">
+                <span>EAN</span>
+                <input type="text" name="form-ean-number">
             </label>
 
-            <label for="form-cleaning" v-if="showCleaningToggle">
-                Rengøring ønskes
-                <input type="checkbox" id="form-cleaning" checked>
+            <label v-if="showCleaningToggle">
+                <span>Rengøring ønskes</span>
+                <input type="checkbox" name="form-cleaning" checked>
             </label>
 
-            <label for="form-arrival-date">
-                Ankomst dato
-                <input type="date" id="form-arrival-date">
+            <label>
+                <span>Ankomst dato</span>
+                <input type="date" name="form-arrival-date">
             </label>
 
-            <label for="form-arrival-time" v-if="showArrivalTime">
-                Ankomst tid
-                <input type="time" id="form-arrival-time">
+            <label v-if="showArrivalTime">
+                <span>Ankomst tid</span>
+                <input type="time" name="form-arrival-time">
             </label>
 
-            <label for="form-departure-date">
-                Afrejse dato
-                <input type="date" id="form-departure-date">
+            <label>
+                <span>Afrejse dato</span>
+                <input type="date" name="form-departure-date">
             </label>
 
-            <label for="form-departure-time" v-if="showDepartureTime">
-                Afrejse tid
-                <input type="time" id="form-departure-time">
+            <label v-if="showDepartureTime">
+                <span>Afrejse tid</span>
+                <input type="time" name="form-departure-time">
             </label>
-
-            <button type="submit">SEND</button>
+            <button type="submit" class="submit-button">Send</button>
         </form>
     </div>
 </template>
@@ -151,3 +149,55 @@
         }
     }
 </script>
+
+<style lang="scss">
+    .form-preview {
+        margin: 20px;
+        padding: 20px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        box-shadow: 0 0 3px rgba(0,0,0,0.5), 0 3px 7px rgba(0,0,0,0.3);
+    }
+
+    .form-preview h1 {
+        font-size: 1.6em;
+    }
+
+    .form-preview h2 {
+        font-size: 1em;
+    }
+
+    .form-preview form {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .form-preview input[type=text] {
+        font-family: inherit;
+    }
+
+    .form-preview button.submit-button {
+        background-color: lighten(#3C6E71, 20%);
+        color:black;
+        width: 100%;
+        padding: 20px;
+        margin: 0.9em 0;
+        border: none;
+        font-size: 1em;
+        font-family: inherit;
+        cursor: pointer;
+        transition: background-color 200ms;
+    }
+
+    .form-preview button.submit-button:hover {
+        background-color: lighten(#3C6E71, 15%);
+    }
+
+
+    .form-preview label {
+        display: flex;
+        margin: 0.4em 0;
+        flex-direction: column;
+    }
+</style>
