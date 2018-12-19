6bb411329fff25afc9e52c7bdf48c55138b7b7d6
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2018-12-19 11:55:15 +0100

    Extract simple labelled-input-field component which was repeated a lot in the form

---
+++ components/form-element/labelled-input-field.vue
@@ -0,0 +1,23 @@
+<template>
+    <label class="labelled-input-field">
+        <span>{{ label }}</span>
+        <input type="text" :placeholder="placeholder" :value="value" @input="onValueChange" :required="required">
+    </label>
+</template>
+
+<script>
+export default {
+    props: ["label", "placeholder", "value", "required"],
+    methods: {
+        onValueChange(event) {
+            this.$emit("input", event.target.value);
+        }
+    }
+};
+</script>
+
+<style>
+.labelled-input-field input {
+    font-family: inherit;
+}
+</style>
