a5345e9fc16f08869454fc2d478022176132306b
ITViking <philip.dein@gmail.com>
2019-04-03 16:21:51 +0200

    Style welcome components

--- components/welcome.vue
+++ components/welcome.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="introductory-text">
+    <div class="welcome-container">
         <div class="headers">
             <h1>Velkommen til HUAS</h1>
-            <h3>Hurtigere og nemmere hytte (ud)lejning for alle parter</h3>
+            <h2>Hurtigere og nemmere hytte (ud)lejning for alle parter</h2>
         </div>
         <div class="short-description">
-                <p>HUAS er et hytte administrations system</p>
-            <p>designet til at gøre dit liv</p>
-            <p>som hytte udlejer så nemt som muligt!</p>
+            <p>HUAS er et hytteadministrationssystem
+            designet til at gøre dit liv
+            som hytte udlejer så nemt som muligt!</p>
         </div>
     </div>
 </template>
@@ -49,15 +49,22 @@ export default {
 
 <style lang="scss">
 
-    .introductory-text {
+    .welcome-container {
         display: flex;
         align-content: center;
         flex-direction: column;
         width: 50vw;
+        background-color: #3C6E71;
+        background-image: linear-gradient(0deg, lighten(#3C6E71, 20%), lighten(#3C6E71, 25%));
+        padding: 100px 40px;
+
+        h1 {
+            font-weight: 500;
+        }
     }
 
     .short-description {
-        align-content: center
+        max-width: 450px;
     }
 
 </style>
