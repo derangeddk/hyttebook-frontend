e4e07123a80066ed53f9f1c01c999aef0ebd5a49
ITViking <philip.dein@gmail.com>
2019-04-03 20:40:55 +0200

    registration only lets user pass on if they registered correctly

--- components/sign-up.vue
+++ components/sign-up.vue
@@ -53,11 +53,15 @@ export default {
                 response = await axios.post('http://localhost:4752/users', payload, { headers });
             } catch(err) {
                 console.error("shit happened while trying to register user: ", err);
+                return;
             }
 
             this.setUsername(this.username);
             this.setHutName(this.hutName);
             this.$router.push("/form-configuration");
+
+
+
         }
     },
     components: { LabelledInput, PrimaryButton, SecondaryButton }
