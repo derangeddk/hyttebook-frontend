babdc8f20b2a70ff1eb420f1d135cae81452280c
ITViking <philip.dein@gmail.com>
2019-04-17 15:02:27 +0200

    nav bar feel more right now

--- components/information-header.vue
+++ components/information-header.vue
@@ -35,10 +35,10 @@ import { mapGetters } from 'vuex';
     nav {
         width: 100%;
         height: 60px;
-        background-color: darken(#3C6E71, 2%);
-        background-image: linear-gradient(0deg, darken(#3C6E71, 2%) 40%, lighten(#3C6E71, 1%));
+        background-color: #3C6E71;
+        background-image: linear-gradient(300deg, lighten(#3C6E71, 17%) 60%, lighten(#3C6E71, 22%));
         text-align: center;
-        color: rgb(230,230,230);
+        color: black;
     }
 
     .header-wrapper {
@@ -58,6 +58,7 @@ import { mapGetters } from 'vuex';
         font-size: 2.3em;
         padding: 0.2em 0.5em;
         margin: 0;
+        font-weight: 500;
     }
 
     .logged-in-user-information {
@@ -66,17 +67,17 @@ import { mapGetters } from 'vuex';
         justify-content:flex-start;
         align-items: center;
         line-height: 100%;
-    }
 
-    .hutname {
-        padding: 0 1.2em;
-        font-weight: 300;
-        font-size: 0.95em;
-    }
+        .hutname {
+            padding: 0 1.2em;
+            font-weight: 300;
+            font-size: 0.95em;
+        }
 
-    .username {
-        padding: 0 1.2em;
-        font-size: 1.1em;
+        .username {
+            padding: 0 1.2em;
+            font-size: 1.1em;
+        }
     }
 </style>
 
