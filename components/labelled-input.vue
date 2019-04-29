16c9314ce1fe9067e34cbf26366a1eb856b1e795
ITViking <philip.dein@gmail.com>
2019-04-29 18:56:12 +0200

    shows guidance and error messages as well. Gives user feedback when input is correct

--- components/labelled-input.vue
+++ components/labelled-input.vue
@@ -5,14 +5,25 @@
             <span class="error-message" v-show="errorMessage">
                 {{ errorMessage }}
             </span>
+            <span class="guidance-message" v-show="guidanceMessage">
+                {{ guidanceMessage }}
+            </span>
         </span>
-        <input :name="name" :type="type" :value="value" autocomplete="new-password" @input="$emit('input', $event.target.value)" @change="$emit('change')">
+        <input v-bind:class="{correct: inputCorrectness}" :name="name" :type="type" :value="value" autocomplete="new-password" @input="$emit('input', $event.target.value)" @change="$emit('change')">
     </label>
 </template>
 
 <script>
 export default {
-    props: [ "name", "type", "label", "value", "errorMessage" ],
+    computed: {
+        inputCorrectness: function() {
+            if(this.value !== "" && !this.errorMessage && !this.guidanceMessage) {
+                return true;
+            }
+            return false;
+        }
+    },
+    props: [ "name", "type", "label", "value", "errorMessage", "guidanceMessage" ],
 }
 </script>
 
@@ -34,6 +45,11 @@ export default {
                 display: inline;
                 color: red;
             }
+
+            .guidance-message {
+                display: inline;
+                color: inherit;
+            }
         }
 
         input {
@@ -43,6 +59,10 @@ export default {
             font-family: "Kanit";
             padding: 3px 4px;
             font-size: 1.2em;
+
+            &.correct {
+                border: 1px solid rgb(0, 179, 0);
+            }
         }
     }
 
