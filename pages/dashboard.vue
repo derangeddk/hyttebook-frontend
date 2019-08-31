e6dd48f4220e1ca9d733315dad7c44a0fb9991cf
ITViking <philip.dein@gmail.com>
2019-08-31 22:59:52 +0200

    Require that users are affiliated with a hut before accessing the dashboard

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -8,6 +8,6 @@
 import InformationHeader from "~/components/information-header";
 export default {
     components: { InformationHeader },
-    middleware: "requireUser",
+    middleware: ["requireUser", "requireHut"],
 }
 </script>
