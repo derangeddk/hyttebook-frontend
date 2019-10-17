86ba44eca04debd4db458fe71a1aa6c761c7eead
ITViking <philip.dein@gmail.com>
2019-10-17 12:24:41 +0200

    session is stored in token and should therefore not be stored anywhere else

--- middleware/requireHut.js
+++ middleware/requireHut.js
@@ -1,10 +1,4 @@
 export default function({store, redirect, app}) {
-    try {
-        store.commit('initialiseCachedUser');
-    } catch (error) {
-        return redirect("/hut-management");
-    }
-
     if(!store.getters.hutName) {
         return redirect("/hut-management");
     }
--- middleware/requireUser.js
+++ middleware/requireUser.js
@@ -1,12 +1,4 @@
 export default function({store, redirect, app}) {
-
-    try {
-        store.commit('initialiseCachedUser');
-    } catch (error) {
-        console.log(error);
-        return redirect("/");
-    }
-
     if(!store.getters.username) {
         return redirect("/");
     }
