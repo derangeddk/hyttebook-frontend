cc3ca1e0636744ad55451fce58f6851bffc2def4
ITViking <philip.dein@gmail.com>
2019-10-18 13:10:23 +0200

    let users avoid having to login if their token is still valid

--- pages/index.vue
+++ pages/index.vue
@@ -15,6 +15,8 @@ import SignUp from '~/components/sign-up';
 import SignIn from '~/components/sign-in';
 import Welcome from '~/components/welcome';
 import HideableContainer from '~/components/hideable-container';
+import axios from 'axios';
+import { mapMutations } from "vuex";
 
 export default {
     head: {
@@ -28,6 +30,34 @@ export default {
         return {
             showSignIn: false,
         };
+    },
+    methods: {
+        ...mapMutations([
+            "setUsername"
+        ])
+    },
+    async mounted() {
+        let response;
+        try {
+            response = await axios.get(
+                "http://localhost:4752/whoami",
+                {
+                    withCredentials: true
+                }
+            );
+        } catch(error) {
+            console.error(error);
+            return;
+        }
+
+        if(!response) return;
+        if(response.status != 200) return;
+
+        this.setUsername(response.data.user.username);
+        //push to hut-mangement, which will check if the user has any huts.
+        this.$router.push("/hut-management");
+
+        return;
     }
 }
 </script>
