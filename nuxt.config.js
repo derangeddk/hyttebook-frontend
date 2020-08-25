1cf1446253427eb8c6f9e00693f2a5849b0199c1
ITViking <philip.dein@gmail.com>
2020-08-26 00:23:27 +0200

    enable theming of the site

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import colors from 'vuetify/es5/util/colors';
+
 module.exports = {
     env: { },
     css: ["~/css/main.scss"],
@@ -5,5 +7,22 @@ module.exports = {
     buildModules: [
       // Simple usage
       '@nuxtjs/vuetify',
-    ]
+    ],
+    vuetify: {
+      customVariables: ['~/assets/variables.scss'],
+      theme: {
+        light: true,
+        themes: {
+          light: {
+            primary: '#66a9ad',
+            accent: colors.grey.lighten3,
+            secondary: colors.amber.lighten3,
+            info: colors.teal.lighten1,
+            warning: colors.amber.base,
+            error: colors.deepOrange.accent4,
+            success: colors.green.accent3
+          }
+        }
+      }
+    }
 }
